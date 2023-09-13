59d89b9da759780af5e6815125aa28ce7a5a6bdf
Kiran Isaacs <kiranisaacs12@gmail.com>
2023-09-13 14:49:05 +0200

    async file system module

---
+++ 11-builtinModules4-FS-async.js
@@ -0,0 +1,32 @@
+const {readFileSync, writeFileSync, readFile, writeFile} = require('fs');
+
+readFile('./content/first.txt','utf-8', (err, result) => {
+    if(err){
+        console.log(err);
+        return;
+    };
+    console.log(result);
+    const first = result;
+
+    readFile('./content/second.txt','utf-8', (err, result) => {
+        if(err){
+            console.log(err);
+            return;
+        };
+        console.log(result);
+        const second = result;
+
+        writeFile(
+            './content/result-async.txt',
+            `Here is the result: ${first}, ${second}\n`, 
+            {flag: 'a'},
+            (err, result) => {
+                if(err){
+                    console.log(err);
+                    return;
+                };
+                console.log(result);
+            })
+
+    })
+})
