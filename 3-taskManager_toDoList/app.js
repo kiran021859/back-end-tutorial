fde425457ea3fed3a68549c364e26e0d8eb0805b
Kiran Isaacs <kiranisaacs12@gmail.com>
2023-09-21 18:39:25 +0200

    database setup and connection/ setup .env file and connect .env to app.js file

--- 3-taskManager_toDoList/app.js
+++ 3-taskManager_toDoList/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+require('./DB/connect');
 const app = express();
 const port = 5000;
-console.log('Task Manager App')
-const tasks = require('./routes/task')
-
+console.log('Task Manager App');
+const tasks = require('./routes/task');
+const connectDB = require('./DB/connect')
+require('dotenv').config() // .env connection
 
 app.use(express.static('./methods-public'))
 app.use(express.urlencoded({extended: false}))
@@ -11,6 +13,15 @@ app.use(express.json())
 
 app.use('/api/v1/tasks', tasks );
 
-app.listen(port, ()=>{
-    console.log(`listening to port:${port}...`);
-})
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, ()=>{
+            console.log(`listening to port:${port}...`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
