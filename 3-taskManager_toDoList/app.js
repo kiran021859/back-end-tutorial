60bd766232edfffc227d2f268db7fd87b4c5873c
Kiran Isaacs <kiranisaacs12@gmail.com>
2023-09-21 14:26:31 +0200

    started task manager/ to do list app

--- 3-taskManager_toDoList/app.js
+++ 3-taskManager_toDoList/app.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 5000;
 console.log('Task Manager App')
+
+app.get('/', (req, res)=>{
+    res.status(200).send('<h1>Helo world</h1>')
+})
+
+
+app.listen(port, ()=>{
+    console.log('listening to port:', port);
+})
