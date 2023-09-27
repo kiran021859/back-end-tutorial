7024dd3e7bb3f08fc33f97cdf96dfb77c894fb12
Kiran Isaacs <kiranisaacs12@gmail.com>
2023-09-27 13:25:59 +0200

    installing express async errors

--- 4-StoreAPI/controllers/products.js
+++ 4-StoreAPI/controllers/products.js
@@ -1,11 +1,14 @@
 
 
 const getAllProducts = async (req, res)=>{
+    throw new Error('testing new errors')
     res.status(200).json({msg:'products test1'})
 };
 
 const getAllProductsStatic = async (req, res)=>{
+    console.log(err);
     res.status(200).json({msg:'products test2'})
+    
 };
 
 module.exports ={ getAllProducts, getAllProductsStatic}; 
