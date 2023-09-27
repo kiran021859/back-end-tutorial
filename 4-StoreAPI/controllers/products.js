5df7f8bdbd291fd34888bb5bd8c64e304bb301e1
Kiran Isaacs <kiranisaacs12@gmail.com>
2023-09-27 14:57:39 +0200

    fixed export error

--- 4-StoreAPI/controllers/products.js
+++ 4-StoreAPI/controllers/products.js
@@ -1,12 +1,12 @@
 
 
 const getAllProducts = async (req, res)=>{
-    throw new Error('testing new errors')
+    throw new Error('testing new errors');
     res.status(200).json({msg:'products test1'})
 };
 
 const getAllProductsStatic = async (req, res)=>{
-    console.log(err);
+    
     res.status(200).json({msg:'products test2'})
     
 };
