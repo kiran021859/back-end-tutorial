734b8d89b755c4a1615306660969b7d9b9fb64e7
Kiran Isaacs <kiranisaacs12@gmail.com>
2023-10-05 11:55:57 +0200

    sort function

--- 4-StoreAPI/controllers/products.js
+++ 4-StoreAPI/controllers/products.js
@@ -2,15 +2,13 @@ const Product = require('../models/product');
 //const { search } = require('../routes/products');
 
 const getAllProductsStatic = async (req, res)=>{
-    let search = 'albany sectional'
-    const products = await Product.find({
-        name: { $regex: search, $options: "i" },
-    })
+    
+    const products = await Product.find({}).sort('-name price')
     res.status(200).json({products, nbHits: products.length})
 };
 
 const getAllProducts = async (req, res)=>{
-    const {featured, company, name} = req.query;
+    const {featured, company, name, sort} = req.query;
     const queryObject = {};
     if (featured) {
         queryObject.featured = featured === 'true'? true : false;
@@ -21,10 +19,19 @@ const getAllProducts = async (req, res)=>{
     if(name){
         queryObject.name = { $regex:name, $options: "i" }
     }
-    
     console.log(queryObject);
-    const products = await Product.find(queryObject);
+    let result = Product.find(queryObject);
+   
+    if(sort){
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+        //products = products.sort();
+        console.log(sortList);
+    }else{
+        result = result.sort('createdAt')
+    }
     //console.log(req.query);
+    const products = await result;
     res.status(200).json({products, nbHits: products.length})
 };
 
