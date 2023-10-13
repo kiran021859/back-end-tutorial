592c16b728a6f6fddacec4c78de2c82e16ec3468
Kiran Isaacs <kiranisaacs12@gmail.com>
2023-10-13 10:09:53 +0200

    limit and page sorting

--- 4-StoreAPI/controllers/products.js
+++ 4-StoreAPI/controllers/products.js
@@ -3,7 +3,10 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res)=>{
     
-    const products = await Product.find({}).select('name price')
+    const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(3)
     res.status(200).json({products, nbHits: products.length})
 };
 
@@ -30,13 +33,17 @@ const getAllProducts = async (req, res)=>{
     }else{
         result = result.sort('createdAt')
     }
-
+//select only sertain fields in the json schema  
     if(fields){
         const fieldsList = fields.split(',').join(' ');
         result = result.select(fieldsList);
     }
 
+const page = Number(req.query.page) || 1;
+const limit = Number(req.query.limit) || 10;
+const skip = (page -1) * limit;
 
+result = result.skip(skip).limit(limit);
 
     //console.log(req.query);
     const products = await result;
