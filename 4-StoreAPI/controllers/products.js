eac7563b3d893b9ce430888f9ecae685f3249b64
Kiran Isaacs <kiranisaacs12@gmail.com>
2023-10-16 09:39:11 +0200

    numeric filtering Price and rating, done with filtering lesson JWT next

--- 4-StoreAPI/controllers/products.js
+++ 4-StoreAPI/controllers/products.js
@@ -3,15 +3,15 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res)=>{
     
-    const products = await Product.find({})
-    .sort('name')
+    const products = await Product.find({price:{ $gt: 30 }})
+    .sort('price')
     .select('name price')
     .limit(3)
     res.status(200).json({products, nbHits: products.length})
 };
 
 const getAllProducts = async (req, res)=>{
-    const {featured, company, name, sort, fields} = req.query;
+    const {featured, company, name, sort, fields, numericFilters } = req.query;
     const queryObject = {};
     if (featured) {
         queryObject.featured = featured === 'true'? true : false;
@@ -22,6 +22,30 @@ const getAllProducts = async (req, res)=>{
     if(name){
         queryObject.name = { $regex:name, $options: "i" }
     }
+    if(numericFilters){
+        const operatorMap = {
+            '>':'$gt',
+            '>=':'$gte',
+            '=':'$eq',
+            '<':'$lt',
+            '<=':'$lte',
+        }
+        const regEx = /\b(>|>=|=|<|<=)\b/g  
+        let filters = numericFilters.replace(regEx,(match)=>`-${operatorMap[match]}-`);
+        const options = ['price', 'rating'];
+        filters = filters.split(',').forEach((item)=>{
+            const [field, operator, value] = item.split('-');
+            if(options.includes(field)){
+                queryObject[field] = {[operator]: Number(value)};
+            };
+        })
+
+        console.log(numericFilters, filters);
+    }
+
+
+
+
     console.log(queryObject);
     let result = Product.find(queryObject);
    
